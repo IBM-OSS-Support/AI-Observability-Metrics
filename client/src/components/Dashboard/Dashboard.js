aca06ba5106b097d0dcd81d55c260827ba163a1e
Jinto Jose <jinto.jose1@ibm.com>
2024-01-12 19:18:21 +0530

    Dashboard updates

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -11,10 +11,11 @@
  ****************************************************************************** */
 import React from "react";
 import {
-  ClickableTile, 
-  Column, 
+  ClickableTile,
+  Column,
   Content,
   Grid,
+  TextInput,
 } from "@carbon/react";
 
 import { ArrowRight } from "@carbon/icons-react";
@@ -23,8 +24,8 @@ import { ArrowRight } from "@carbon/icons-react";
 import { DARK_THEME_ILLUSTRATION } from './media/dark-theme-illustration';
 import CustomLineChart from "../common/CustomLineChart";
 
-import { 
-  callCountData, 
+import {
+  callCountData,
   callCountOptions,
   latencyData,
   latencyDistData,
@@ -40,27 +41,50 @@ function Dashboard() {
     >
       <Grid
         fullWidth
-        narrow
-        id="header"
-        className="header"
+        className="page-content"
       >
-        <Column max={8} xlg={8} lg={8} md={8} sm={4}>
-          <div className="left">
-            <div className="title">
-              <h3>
-                Welcome back, <br/> Alice
-              </h3>
-            </div>
-            {/* <div className="quick-actions">
+        <Column max={12} xlg={12} lg={12} md={8} sm={4} >
+          <Grid
+            fullWidth
+            narrow
+            id="header"
+            className="header"
+          >
+            <Column max={6} xlg={6} lg={6} md={8} sm={4}>
+              <div className="left">
+                <div className="title">
+                  <h3>
+                    Welcome back, <br /> Alice
+                  </h3>
+                </div>
+              </div>
+            </Column>
+            {/* <Column max={1} xlg={1} lg={1} md={1} sm={0} /> */}
+            <Column max={6} xlg={6} lg={6} md={0} sm={0}>
+              <div className="right">
+                <img
+                  src={DARK_THEME_ILLUSTRATION}
+                  alt="illustration"
+                />
+              </div>
+            </Column>
+          </Grid>
+          <Grid
+            // fullWidth
+            narrow
+            id="body"
+            className="body"
+          >
+            <Column max={4} xlg={4} lg={4} md={4} sm={4} className="content-tile">
               <ClickableTile
                 className="quick-action"
                 style={{}}
-                href={'#/'}
+                href={'#/traces'}
                 onKeyDown={(event) => { }}
               >
                 <div>
                   <h6>
-                    name
+                    Traces
                   </h6>
                   <p>
                     description
@@ -71,63 +95,96 @@ function Dashboard() {
                   <ArrowRight size={14} />
                 </div>
               </ClickableTile>
-            </div> */}
+            </Column>
+            <Column max={4} xlg={4} lg={4} md={4} sm={4} className="content-tile">
+              <ClickableTile
+                className="quick-action"
+                style={{}}
+                href={'#/sessions'}
+                onKeyDown={(event) => { }}
+              >
+                <div>
+                  <h6>
+                    Sessions
+                  </h6>
+                  <p>
+                    description
+                  </p>
+                </div>
+                <div className="destination">
+                  more
+                  <ArrowRight size={14} />
+                </div>
+              </ClickableTile>
+            </Column>
+            <Column max={4} xlg={4} lg={4} md={4} sm={4} className="content-tile">
+              <ClickableTile
+                className="quick-action"
+                style={{}}
+                href={'#/metrics'}
+                onKeyDown={(event) => { }}
+              >
+                <div>
+                  <h6>
+                    Metrics
+                  </h6>
+                  <p>
+                    description
+                  </p>
+                </div>
+                <div className="destination">
+                  more
+                  <ArrowRight size={14} />
+                </div>
+              </ClickableTile>
+            </Column>
+            <Column max={16} xlg={16} lg={16} md={8} sm={4} className="content-tile">
+              <ClickableTile
+                // className="chart-tile"
+                style={{}}
+                href={'#/'}
+                onKeyDown={(event) => { }}
+              >
+                <CustomLineChart
+                  data={callCountData}
+                  options={callCountOptions}
+                />
+              </ClickableTile>
+            </Column>
+            <Column max={8} xlg={8} lg={8} md={8} sm={4} className="content-tile">
+              <ClickableTile
+                // className="chart-tile"
+                style={{}}
+                href={'#/'}
+                onKeyDown={(event) => { }}
+              >
+                <CustomLineChart
+                  data={latencyData}
+                  options={latencyOptions}
+                />
+              </ClickableTile>
+            </Column>
+            <Column max={8} xlg={8} lg={8} md={8} sm={4} className="content-tile">
+              <ClickableTile
+                // className="chart-tile"
+                style={{}}
+                href={'#/'}
+                onKeyDown={(event) => { }}
+              >
+                <CustomLineChart
+                  data={latencyDistData}
+                  options={latencyDistOptions}
+                />
+              </ClickableTile>
+            </Column>
+          </Grid>
+        </Column>
+        <Column max={4} xlg={4} lg={4} md={8} sm={4} className="chat-container" >
+          <div className="chat-box">
+            <h5 className="title">Talk to Roja</h5>
+            <TextInput placeholder="What do you want to ask?"></TextInput>
+            <label className="chat-info">Roja may generate incorrect information. <br/>Verify important information.</label>
           </div>
-        </Column>
-        <Column max={2} xlg={2} lg={2} md={1} sm={0} />
-        <Column max={6} xlg={6} lg={6} md={0} sm={0}>
-          <div className="right">
-            <img
-              src={DARK_THEME_ILLUSTRATION}
-              alt="illustration"
-            />
-          </div>
-        </Column>
-      </Grid>
-      <Grid
-        // fullWidth
-        narrow
-        id="body"
-        className="body"
-      >
-        <Column max={16} xlg={16} lg={16} md={8} sm={4} className="content-tile">
-          <ClickableTile
-            // className="chart-tile"
-            style={{}}
-            href={'#/'}
-            onKeyDown={(event) => { }}
-          >
-            <CustomLineChart
-              data={callCountData}
-              options={callCountOptions}
-            />
-          </ClickableTile>
-        </Column>
-        <Column max={8} xlg={8} lg={8} md={8} sm={4} className="content-tile">
-          <ClickableTile
-            // className="chart-tile"
-            style={{}}
-            href={'#/'}
-            onKeyDown={(event) => { }}
-          >
-            <CustomLineChart
-              data={latencyData}
-              options={latencyOptions}
-            />
-          </ClickableTile>
-        </Column>
-        <Column max={8} xlg={8} lg={8} md={8} sm={4} className="content-tile">
-          <ClickableTile
-            // className="chart-tile"
-            style={{}}
-            href={'#/'}
-            onKeyDown={(event) => { }}
-          >
-            <CustomLineChart
-              data={latencyDistData}
-              options={latencyDistOptions}
-            />
-          </ClickableTile>
         </Column>
       </Grid>
     </Content>
