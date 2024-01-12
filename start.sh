460c86164a0a29726f5d527406ea02b091d121a0
Abdulla Thanseeh <abdulla.thanseeh@ibm.com>
2024-01-12 09:27:48 +0530

    api endpoints

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 docker build -t roja-metric-react-app client
 
-docker run -p 9000:80 roja-metric-react-app
+docker run -d -p 9000:80 roja-metric-react-app
 
 docker build -t roja-metric-backend-app server
 
-docker run -p 5000:5000 roja-metric-backend-app
+docker run -d -p 5000:5000 roja-metric-backend-app
