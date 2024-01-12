659d62d3d7b514f1a662a3c6c4a295860059c86e
Abdulla Thanseeh <abdulla.thanseeh@ibm.com>
2024-01-12 17:26:58 +0530

    influx db changes

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@ docker build -t roja-metric-react-app client
 
 docker run -d -p 9000:80 roja-metric-react-app
 
+docker build -t roja-metric-backend-db db
+
+docker run -d -p 8086:8086 roja-metric-backend-db
+
 docker build -t roja-metric-backend-app server
 
 docker run -d -p 6000:6000 roja-metric-backend-app
