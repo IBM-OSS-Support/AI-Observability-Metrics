21ebe73f5464c287d76625bfdbefe21c440ff12e
Abdulla Thanseeh <abdulla.thanseeh@ibm.com>
2024-01-14 11:32:21 +0530

    start script changes

--- start.sh
+++ start.sh
@@ -1,11 +1,27 @@
+#!/bin/bash
+
+# Function to stop and remove a container if it exists
+stop_and_remove_container() {
+    CONTAINER_NAME=$1
+    if [ $(docker ps -a -q -f name=^/${CONTAINER_NAME}$) ]; then
+        docker stop ${CONTAINER_NAME}
+        docker rm ${CONTAINER_NAME}
+    fi
+}
+
+# Build and run roja-metric-react-app
 docker build -t roja-metric-react-app client
+stop_and_remove_container roja-metric-react-app
+docker run -d -p 9000:80 --name roja-metric-react-app roja-metric-react-app
 
-docker run -d -p 9000:80 roja-metric-react-app
-
+# Build and run roja-metric-backend-db
 docker build -t roja-metric-backend-db db
+stop_and_remove_container roja-metric-backend-db
+docker run -d -p 8086:8086 --name roja-metric-backend-db roja-metric-backend-db
 
-docker run -d -p 8086:8086 roja-metric-backend-db
-
+# Build and run roja-metric-backend-app
 docker build -t roja-metric-backend-app server
+stop_and_remove_container roja-metric-backend-app
+docker run -d -p 6000:6000 --name roja-metric-backend-app roja-metric-backend-app
 
-docker run -d -p 6000:6000 roja-metric-backend-app
+echo "Containers built and started successfully."
