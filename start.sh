4356dc89e99f602b0d05c3764d15659573accd4f
Abdulla Thanseeh <abdulla.thanseeh@ibm.com>
2024-01-14 11:36:09 +0530

    start script changes

--- start.sh
+++ start.sh
@@ -15,9 +15,9 @@ stop_and_remove_container roja-metric-react-app
 docker run -d -p 9000:80 --name roja-metric-react-app roja-metric-react-app
 
 # Build and run roja-metric-backend-db
-docker build -t roja-metric-backend-db db
+docker build -t roja-metric-backend-db db/postgres
 stop_and_remove_container roja-metric-backend-db
-docker run -d -p 8086:8086 --name roja-metric-backend-db roja-metric-backend-db
+docker run -d -p 5432:5432 --name roja-metric-backend-db roja-metric-backend-db
 
 # Build and run roja-metric-backend-app
 docker build -t roja-metric-backend-app server
