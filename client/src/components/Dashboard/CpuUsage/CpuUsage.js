0883fa2b1b5066a272cff53541e843659b3b301a
Jinto Jose <jinto.jose1@ibm.com>
2024-01-17 15:39:44 +0530

    CPU Usage tile updates

--- client/src/components/Dashboard/CpuUsage/CpuUsage.js
+++ client/src/components/Dashboard/CpuUsage/CpuUsage.js
@@ -9,18 +9,15 @@
  * of its trade secrets, irrespective of what has been deposited with
  * the U.S. Copyright Office.
  ****************************************************************************** */
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 // Components ----------------------------------------------------------------->
 import { Tile } from "@carbon/react";
 import { GaugeChart } from "@carbon/charts-react";
-
-const data = [
-  {
-    group: 'value',
-    value: 1.02
-  }
-]
+import { getAppData } from "../../../appData";
+import { useStoreContext } from "../../../store";
+import Loader from "../../common/Loader";
 
 const options = {
   theme: "g90",
@@ -48,20 +45,70 @@ const options = {
 }
 
 const CpuUsage = () => {
+
+  const [data, setData] = useState([
+    {
+      group: 'value',
+      value: 0
+    }
+  ]);
+  const [avg, setAvg] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const { state } = useStoreContext();
+
+  useEffect(() => {
+    let newData = [
+      {
+        group: 'value',
+        value: 0
+      }
+    ];
+    let newAvg = 0;
+    if(state.status === 'success') {
+      const appData = getAppData();
+
+      const cpuUsages = appData
+        .filter(d => moment(d.data.upload_ms / 1000).diff(moment(), 'days') <= 7)
+        .map(d => {
+          const cpuUsage = d.data.metrics.find(m => m.name === 'process_cpu_usage');
+          let gauge = 0;
+          if (cpuUsage) {
+            gauge = (cpuUsage.gauge || 0)
+          }
+          return gauge
+        });
+      newData = [
+        {
+          group: 'value',
+          value: cpuUsages[0] || 0
+        }
+      ];
+      newAvg = (cpuUsages.reduce((s, g) => s + +g, 0) / cpuUsages.length).toFixed(2);
+    }
+
+    setData(newData);
+    setAvg(newAvg);
+    setLoading(state.status !== 'success')
+  }, [state.status]);
+
   // Render
   return (
-    <Tile className="infrastructure-components cpu-usage">
-      <h5>Latest CPU usage</h5>
-      <div className="cpu-usage-chart">
-        <GaugeChart
-          data={data}
-          options={options}
-        />
-      </div>
-      <div className="cpu-usage-data">
-        <div className="label">Average CPU usage for last 7 days</div>
-        <h3 className="data">0.79 %</h3>
-      </div>
+    <Tile className="infrastructure-components cpu-usage" >
+      <h5>Latest CPU usage</h5> 
+        <div className="cpu-usage-chart">
+          <GaugeChart
+            data={data}
+            options={options}
+          />
+        </div>
+        <div className="cpu-usage-data">
+          <div className="label">Average CPU usage for last 7 days</div>
+          <h3 className="data">{avg} %</h3>
+        </div>
+        {
+          loading && <Loader />
+        }
     </Tile>
   );
 };
