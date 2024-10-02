33d5c6e95afe671a1134a9892fdda3666c98ae37
athirakm <Athira.K.M@ibm.com>
2024-10-02 09:41:38 -0700

    working code with HOST_IP set from command line when running container

--- docker/commands.sh
+++ docker/commands.sh
@@ -3,6 +3,11 @@
 echo $REACT_APP_BACKEND_API_URL
 echo $FLASK_SERVER_URL
 echo $API_URL
+
+
+export REACT_APP_BACKEND_API_URL="http://$HOST_IP:5000/data"
+echo "Backend API URL set to: $REACT_APP_BACKEND_API_URL"
+
 # Start PostgreSQL
 service postgresql start
 
@@ -19,7 +24,7 @@ cd /app/client-app && npm start &
 
 
 #GRAPHSIGNAL_API_KEY="7e6ff4494810b4cb37255d369cfae983" OPENAI_API_KEY="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" API_URL="http://localhost:3001" FLASK_SERVER_URL="http://127.0.0.1:3001" OPENAI_ORG_ID="org-zvKGqHl0adRlJbe7lpb7n1wX" python3 /app/applications/bhoomaiah_app.py 
-python3 /app/applications/bhoomaiah_app.py
+python3 /app/applications/random_app.py
 #
 sleep infinity
 
