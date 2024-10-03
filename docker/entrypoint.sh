28a325f58506c7be41deb73f0dc06efeeb149f81
athirakm <Athira.K.M@ibm.com>
2024-10-03 03:43:31 -0700

    cleaning dockerfile

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export REACT_APP_BACKEND_API_URL="http://$HOST_IP_FOR_API_SERVER:5000/data"
+# Start PostgreSQL
+service postgresql start
+
+#start flaskapp
+cd /app/applications/flask_server  && flask run --host=0.0.0.0 --port=3001 &
+# Start WebSocket server
+cd /app/websocket-server && node server.js &
+
+#start client-app
+cd /app/client-app && npm start & 
+python3 /app/applications/random_app.py
+#
+sleep infinity
+
